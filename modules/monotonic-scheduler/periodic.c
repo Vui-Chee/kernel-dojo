041449bd73e0415d5d095652a0f44698c37cd199
Vui-Chee <vuicheesiew@gmail.com>
2026-03-15 15:36:51 +0800

    fix(periodic.c): lint

--- modules/monotonic-scheduler/periodic.c
+++ modules/monotonic-scheduler/periodic.c
@@ -3,28 +3,24 @@
 #include <sched.h>
 #include <unistd.h>
 
-void __register(int pid, int period, int p_time) {
-	// TODO: write op to /proc/monotonic_sched/status
-	// REGISTRATION: R,PID,PERIOD,COMPUTATION
-	// DE-REGISTRATION: D,PID
-	// YIELD: Y,PID
-}
+void __register(int pid, int period, int p_time) {}
 
 void __deregister(int pid) {}
 
 // writes to the proc filesystem, kernel module puts application to sleep
-void __yield() {}
+void __yield(void) {}
 
-void __read_fs() {}
+void __read_fs(void) {}
 
-int __job(int n) {
-	if (n <= 1) {
+int __job(int n)
+{
+	if (n <= 1)
 		return 1;
-	}
 	return n * __job(n - 1);
 }
 
-int main(void) {
+int main(void)
+{
 	int p_time = 3000; // ms
 	int period = 100; // ms
 	int pid = getpid();
@@ -44,23 +40,23 @@ int main(void) {
 }
 
 /*
-	REGISTER(pid, period, processing_time);
-	// Read ProcFS: Verify the process was admitted 
-	list = READ(ProcFS); 
-	if (!process in the list) exit(1);
-	// setup everything needed for RT loop
-	t0 = clock_gettime();
-	// Proc filesystem
-	YIELD(PID); 
-	// this is the real-time loop
-	while (exist jobs)
-	{
-		wakeup_time = clock_gettime() - t0;
-		// factorial computation
-		do_job();
-		process_time = clock_gettime() - wakeup_time;
-		YIELD(PID); 
-	}
-	// Interact with ProcFS
-	DEREGISTER(PID); 
-*/
+ * REGISTER(pid, period, processing_time);
+ * // Read ProcFS: Verify the process was admitted
+ * list = READ(ProcFS);
+ * if (!process in the list) exit(1);
+ * // setup everything needed for RT loop
+ * t0 = clock_gettime();
+ * // Proc filesystem
+ * YIELD(PID);
+ * // this is the real-time loop
+ * while (exist jobs)
+ * {
+ *	wakeup_time = clock_gettime() - t0;
+ *	// factorial computation
+ *	do_job();
+ *	process_time = clock_gettime() - wakeup_time;
+ *	YIELD(PID);
+ * }
+ * // Interact with ProcFS
+ * DEREGISTER(PID);
+ */
