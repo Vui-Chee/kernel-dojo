2085b4dac5db94467f99c30a66daf8416b719ae9
Vui-Chee <vuicheesiew@gmail.com>
2026-03-15 15:05:53 +0800

    bare bones userapp for testing

---
+++ modules/monotonic-scheduler/periodic.c
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <stdio.h>
+#include <sched.h>
+#include <unistd.h>
+
+void __register(int pid, int period, int p_time) {
+	// TODO: write op to /proc/monotonic_sched/status
+	// REGISTRATION: R,PID,PERIOD,COMPUTATION
+	// DE-REGISTRATION: D,PID
+	// YIELD: Y,PID
+}
+
+void __deregister(int pid) {}
+
+// writes to the proc filesystem, kernel module puts application to sleep
+void __yield() {}
+
+void __read_fs() {}
+
+int __job(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return n * __job(n - 1);
+}
+
+int main(void) {
+	int p_time = 3000; // ms
+	int period = 100; // ms
+	int pid = getpid();
+
+	__register(pid, period, p_time);
+
+	// TODO: read() from procfile to verify process is added
+
+	__job(100);
+	printf("sleeping...\n");
+	sleep(10);
+
+	__deregister(pid);
+
+	printf("finished\n");
+	return 0;
+}
+
+/*
+	REGISTER(pid, period, processing_time);
+	// Read ProcFS: Verify the process was admitted 
+	list = READ(ProcFS); 
+	if (!process in the list) exit(1);
+	// setup everything needed for RT loop
+	t0 = clock_gettime();
+	// Proc filesystem
+	YIELD(PID); 
+	// this is the real-time loop
+	while (exist jobs)
+	{
+		wakeup_time = clock_gettime() - t0;
+		// factorial computation
+		do_job();
+		process_time = clock_gettime() - wakeup_time;
+		YIELD(PID); 
+	}
+	// Interact with ProcFS
+	DEREGISTER(PID); 
+*/
